d216742806cfbbbb5ef0787e24ab9f6e35477f25
Pedro Melo <vieirapcm@gmail.com>
2019-05-14 00:56:28 -0300

    CRUD completo

--- api/methods/Users.js
+++ api/methods/Users.js
@@ -7,16 +7,37 @@ module.exports = function(application) {
     
     // GET USERS
     application.get("/users", (req, res) => {
-        var returnValues = [];
         var usersRef = ref; //.child("users")
 
-        usersRef.on("value", (snapshot) => {
-            returnValues = snapshot;
-            res.send(returnValues);
-        });
+        usersRef.on("value", 
+            (snapshot) => {
+                res.json(snapshot.val());
+                usersRef.off("value");
+            },
+            (errorObject) => {
+                console.log(`The read failed: ${errorObject.code}`);
+                res.send(`The read failed: ${errorObject.code}`);
+            });
     });
 
-    // POST USER
+    // GET USER
+    application.get("/user", (req, res) => {
+        var userId = req.query.userId;
+
+        var usersRef = ref.child(`/users/${userId}/`);
+
+        usersRef.on("value", 
+            (snapshot) => {
+                res.json(snapshot.val());
+                usersRef.off("value");
+            },
+            (errorObject) => {
+                console.log(`The read failed: ${errorObject.code}`);
+                res.send(`The read failed: ${errorObject.code}`);
+            });
+    });
+
+    // INSERT USER
     application.post("/users", (req, res) => {
         var data = req.body;
 
@@ -37,13 +58,66 @@ module.exports = function(application) {
             res.send(msg);
             return false;
         }
-        
-        var usersRef = ref.child("users");
 
-        usersRef = usersRef.push({
+        const id = parseInt(Math.random() * 1000000000, 10);
+        
+        var usersRef = ref.child(`/users/${id}/`);
+
+        usersRef = usersRef.set({
             name     : data.name,
             lastName : data.lastName,
+        },
+        (error) => {
+            if (error) {
+                res.send(`Data could not be inserted. Error: ${error}`);
+            } else {
+                res.send(`${data.name} ${data.lastName} registered.`);
+            }
         });
-        res.send(data.name + " " + data.lastName + " registered.");
+    });
+
+    // UPDATE USER
+    application.patch("/users", (req, res) => {
+        var data = req.body;
+
+        var userId = req.query.userId;        
+
+        if (userId == '' || userId == 'undefined') {
+            res.send("The field userID cannot be null");
+            return false;
+        }
+        // TODO: Utilizar apenas os campos enviados pro update
+        var usersRef = ref.child(`/users/${userId}/`);
+
+        usersRef = usersRef.update({
+            name     : data.name,
+            lastName : data.lastName,
+        },
+        (error) => {
+            if (error) {
+                res.send(`Data could not be inserted. Error: ${error}`);
+            } else {
+                res.send(`${data.name} ${data.lastName} updated.`);
+            }
+        });
+    });
+
+    // DELETE USER
+    application.delete('/users', (req, res) => {
+         var userId = req.query.userId; 
+
+        if (userId == '' || userId == 'undefined') {
+            res.send("userID cannot be null");
+            return false;
+        }
+
+        var usersRef = ref.child(`/users/${userId}/`);
+        usersRef.remove()
+            .then(() => {
+                res.send(`User removed`);
+            })
+            .catch((error) => {
+                res.send(`Fail: ${error}`)
+            });
     });
 }
